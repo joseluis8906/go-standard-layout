0e12e686b88de1b93ecff3ac011fc61ea8817df8
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2022-07-21 21:15:11 -0500

    default comment

--- internal/infrastructure/inmemory/post_repository.go
+++ internal/infrastructure/inmemory/post_repository.go
@@ -2,23 +2,34 @@ package inmemory
 
 import (
 	"context"
+	"sync"
 
 	"github.com/joseluis8906/go-standard-layout/internal/domain/post"
+	"github.com/joseluis8906/go-standard-layout/pkg/errors"
 	"github.com/joseluis8906/go-standard-layout/pkg/eventbus"
 )
 
+const (
+	// ErrNotFound ...
+	ErrNotFound = errors.Error("error post not found")
+)
+
 type (
+	// PostRepository ...
 	PostRepository struct {
 		data map[post.PostID]post.Post
+		mux  sync.RWMutex
 	}
 )
 
+// NewPostRepository ...
 func NewPostRepository() *PostRepository {
 	return &PostRepository{
 		data: map[post.PostID]post.Post{},
 	}
 }
 
+// Persist ...
 func (r *PostRepository) Persist(ctx context.Context, p post.Post) (err error) {
 	defer func() {
 		if err != nil {
@@ -29,11 +40,18 @@ func (r *PostRepository) Persist(ctx context.Context, p post.Post) (err error) {
 		eventbus.Publish(ctx, p.EventIDs())
 	}()
 
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
 	r.data[p.ID()] = p
 	return nil
 }
 
+// GetAll ...
 func (r *PostRepository) GetAll(ctx context.Context) ([]post.Post, error) {
+	r.mux.RLock()
+	defer r.mux.RUnlock()
+
 	posts := []post.Post{}
 	for _, p := range r.data {
 		posts = append(posts, p)
@@ -42,6 +60,15 @@ func (r *PostRepository) GetAll(ctx context.Context) ([]post.Post, error) {
 	return posts, nil
 }
 
+// GetByID ...
 func (r *PostRepository) GetByID(ctx context.Context, id post.PostID) (post.Post, error) {
-	return r.data[id], nil
+	r.mux.RLock()
+	defer r.mux.RUnlock()
+
+	p, ok := r.data[id]
+	if !ok {
+		return post.NoopPost(), ErrNotFound
+	}
+
+	return p, nil
 }
